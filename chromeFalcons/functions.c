92bd224cf2c010ae9199fa094f843e720025431a
David Jones <jonesdav004@alpine.local>
2018-10-26 14:15:19 -0600

    hey

--- chromeFalcons/functions.c
+++ chromeFalcons/functions.c
@@ -28,6 +28,27 @@ void driveFunc(int power1, int power2){
 	motor[right3] = power2;
 }
 
+//drive forward
+void driveForward(){
+	driveFunc(127,127);
+}
+
+void driveBack(){
+	driveFunc(-127,-127);
+}
+
+void driveTurnLeft(){
+	driveFunc(-127,127);
+}
+
+void driveTurnRight(){
+	driveFunc(127,-127);
+}
+
+void driveStop(){
+	driveFunc(0,0);
+}
+
 //Lift Function
 void liftFunc(int power){
 	motor[liftLeft] = power;
@@ -133,21 +154,34 @@ void drive(){
 	driveFunc(vexRT[Ch3],vexRT[Ch2]);
 }
 
-void driveForward(){
-	driveFunc(127,127);
+void autonForward(){
+	while((leftDriveEncode()<1500)&&(rightDriveEncode()<1500)){
+		driveForward();
+	}
+	driveStop();
 }
 
-void driveBack(){
-	driveFunc(-127,-127);
+void autonTurnLeft(){
+	while((leftDriveEncode()>-1500)&&(rightDriveEncode()<1500)){
+		driveTurnLeft();
+	}
+	driveStop();
 }
 
-void driveTurnLeft{
-	driveFunc(-127,127);
+void autonBack(){
+	while((leftDriveEncode()>-1500)&&(rightDriveEncode()>-1500)){
+		driveBack();
+	}
+	driveStop();
 }
 
-void driveTurnRight{
-	driveFunc(127,-127);
+void autonForward2(){
+	while((leftDriveEncode()<1500)&&(rightDriveEncode()<1500)){
+		driveForward();
+	}
+	driveStop();
 }
+
 //LCD Functions
 void batteryLCD(){
 
@@ -174,4 +208,179 @@ void batteryLCD(){
 	//Clear LCD
 	clearLCDLine(0);
 	clearLCDLine(1);
+}
+
+//here is the entire auton program/functions
+void auton(){
+	autonForward();
+	autonTurnLeft();
+	autonBack();
+	autonForward2();
+}
+
+//LCD Functions
+//Wait for Press--------------------------------------------------
+void waitForPress(){
+	while(nLCDButtons == 0){}
+	wait1Msec(5);
+}
+//----------------------------------------------------------------
+
+//Wait for Release------------------------------------------------
+void waitForRelease() {
+	while(nLCDButtons != 0){}
+	wait1Msec(5);
+}
+//----------------------------------------------------------------
+
+void lcd(){
+	/*
+Code Chooser
+ROBOTC on VEX 2.0 Cortex
+
+This program uses the Display functions of ROBOTC on the VEX 2.0 Cortex platform.
+It allows the user to choose from 4 different pieces of code using the left and right buttons
+on the VEX LCD. Once the center button is pressed, the code corresponding with the choice is run.
+This code can be adapted for competition based settings - just place the code for the first
+switch case in the pre_auton function, and the code for the second switch in the autonomous task.
+Replace the basic movement behaviors in the second switch with your own autonomous routines.
+
+ROBOT CONFIGURATION
+MOTORS &amp; SENSORS:
+[I/O Port]                    [Name]                            [Type]                                [Description]
+UART Port 2                    none                                VEX LCD                                VEX LCD Screen
+Motor Port 2                rightMotor                    VEX 3-wire module            Right side motor
+Motor Port 3                leftMotor                        VEX 3-wire module            Left side motor
+*/
+ //lcd variables and functions
+const short leftButton = 1;
+const short centerButton = 2;
+const short rightButton = 4;
+
+//Declare count variable to keep track of our choice
+int count = 0;
+//------------- Beginning of User Interface Code ---------------
+//Clear LCD
+	clearLCDLine(0);
+	clearLCDLine(1);
+	//Loop while center button is not pressed
+	while(nLCDButtons != centerButton){
+	//Switch case that allows the user to choose from 4 different options
+		switch(count){
+		case 0:
+		//Display first choice
+		displayLCDCenteredString(0, "Autonomous 1");
+		displayLCDCenteredString(1, "<         Enter        >");
+		waitForPress();
+		//Increment or decrement "count" based on button press
+		if(nLCDButtons == leftButton){
+			waitForRelease();
+			count = 3;
+		}
+		else if(nLCDButtons == rightButton){
+			waitForRelease();
+			count++;
+		}
+		break;
+		case 1:
+		//Display second choice
+		displayLCDCenteredString(0, "Autonomous 2");
+		displayLCDCenteredString(1, "<         Enter        >");
+		waitForPress();
+		//Increment or decrement "count" based on button press
+		if(nLCDButtons == leftButton){
+			waitForRelease();
+			count--;
+		}
+		else if(nLCDButtons == rightButton){
+			waitForRelease();
+			count++;
+		}
+		break;
+		case 2:
+		//Display third choice
+		displayLCDCenteredString(0, "Autonomous 3");
+		displayLCDCenteredString(1, "<         Enter        >");
+		waitForPress();
+		//Increment or decrement "count" based on button press
+		if(nLCDButtons == leftButton){
+			waitForRelease();
+			count--;
+		}
+		else if(nLCDButtons == rightButton){
+			waitForRelease();
+			count++;
+		}
+		break;
+		case 3:
+		//Display fourth choice
+		displayLCDCenteredString(0, "Autonomous 4");
+		displayLCDCenteredString(1, "<         Enter        >");
+		waitForPress();
+		//Increment or decrement "count" based on button press
+		if(nLCDButtons == leftButton){
+			waitForRelease();
+			count--;
+		}
+		else if(nLCDButtons == rightButton){
+			waitForRelease();
+			count = 0;
+		}
+		break;
+		default:
+		count = 0;
+		break;
+		}
+	}
+//------------- End of User Interface Code ---------------------
+
+//------------- Beginning of Robot Movement Code ---------------
+//Clear LCD
+clearLCDLine(0);
+clearLCDLine(1);
+//Switch Case that actually runs the user choice
+switch(count){
+case 0:
+//If count = 0, run the code correspoinding with choice 1
+displayLCDCenteredString(0, "Autonomous 1");
+displayLCDCenteredString(1, "is running!");
+wait1Msec(2000);                        // Robot waits for 2000 milliseconds
+
+// Move forward at full power for 3 seconds
+//Ends before moving on
+
+break;
+case 1:
+//If count = 1, run the code correspoinding with choice 2
+displayLCDCenteredString(0, "Autonomous 2");
+displayLCDCenteredString(1, "is running!");
+wait1Msec(2000);                        // Robot waits for 2000 milliseconds
+
+//robot call void Red
+break;
+case 2:
+//If count = 2, run the code correspoinding with choice 3
+displayLCDCenteredString(0, "Autonomous 3");
+displayLCDCenteredString(1, "is running!");
+wait1Msec(2000);                        // Robot waits for 2000 milliseconds
+
+//Robot call Void Blue
+
+break;
+case 3:
+//If count = 3, run the code correspoinding with choice 4
+displayLCDCenteredString(0, "Autonomous 4");
+displayLCDCenteredString(1, "is running!");
+wait1Msec(2000);                        // Robot waits for 2000 milliseconds
+
+//auton function call void 4
+
+break;
+default:
+displayLCDCenteredString(0, "No valid choice");
+displayLCDCenteredString(1, "was made!");
+break;
+}
+//------------- End of Robot Movement Code -----------------------
+}
 }
