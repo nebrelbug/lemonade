1b96110fbc2f1ec521a441c9104d6ec0fc496a41
acetousk <acetousk@gmail.com>
2018-11-29 19:02:51 -0700

    yv

--- leftPID.c
+++ leftPID.c
@@ -31,21 +31,12 @@ static int   pdRunning = 1;
 static float pdRequestedValue;
 
 
-/*-----------------------------------------------------------------------------*/
-/*
-*/
-/*  pid control task
-*/
-/*
-*/
-/*-----------------------------------------------------------------------------*/
-
-
 // These could be constants but leaving
 // as variables allows them to be modified in the debugger "live"
 float  pd_Kp = 2.0;
 float  pd_Kd = 0.0;
 
+
 /*-----------------------------------------------------------------------------*/
 /*
 */
@@ -54,6 +45,59 @@ float  pd_Kd = 0.0;
 /*
 */
 /*-----------------------------------------------------------------------------*/
+
+task leftPIDController(){
+
+ float  pidSensorCurrentValue;
+ float  pidError;
+ float  pidLastError;
+ float  pidDerivative;
+ float  pidDrive;
+
+ // If we are using an encoder then clear it
+ if(SensorType[ LEFT_SENSOR_INDEX ] == sensorQuadEncoder )
+   SensorValue[ LEFT_SENSOR_INDEX ] = 0;
+
+  	// Init the variables - thanks Glenn :)
+  pidLastError  = 0;
+
+  while( true ){
+    // Is PID control active ?
+    if( pidRunning ){
+      // Read the sensor value and scale
+      pidSensorCurrentValue = SensorValue[ LEFT_SENSOR_INDEX ] * PID_SENSOR_SCALE;
+
+      // calculate error
+      pidError = pidSensorCurrentValue - pidRequestedValue;
+
+        // calculate the derivative
+        pidDerivative = pidError - pidLastError;
+        pidLastError  = pidError;
+
+        // calculate drive
+        pidDrive = (pid_Kp * pidError) + (pid_Kd * pidDerivative);
+
+        // limit drive
+        if( pidDrive > PID_DRIVE_MAX )
+          pidDrive = PID_DRIVE_MAX;
+        if( pidDrive < PID_DRIVE_MIN )
+          pidDrive = PID_DRIVE_MIN;
+
+            // send to motor
+           	leftFunc(pidDrive * PID_MOTOR_SCALE);
+    }else{
+       // clear all
+       pidError      = 0;
+       pidLastError  = 0;
+       pidDerivative = 0;
+       leftFunc(0);
+       rightFunc(0);
+     }
+
+    // Run at 50Hz
+    wait1Msec( 25 );
+  }
+}
 
 task rightPIDController(){
 
@@ -121,8 +165,10 @@ void drivePID(int clicks, int clicks2){
 	// send the motor off somewhere
   pidRequestedValue = clicks;
   pdRequestedValue=clicks2;
+
 	// start the PID task
   startTask( leftPIDController );
+	startTask( rightPIDController );
 
   // use joystick to modify the requested position
   while( true ){
